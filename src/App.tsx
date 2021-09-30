221b59342513950479e83462eb60799ab2aff8bd
Lawrence Croiden Lobo <llobo@planview.com>
2021-09-30 22:28:57 +0530

    Add react router for add meeting page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import * as React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
+import AddMeeting from "./views/addmeeting";
 import Main from "./views/main";
+
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route path="/add-meeting">
+            <AddMeeting />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
